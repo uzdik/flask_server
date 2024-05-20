e3be0bacd85383d401eb221645bcc1b3a22fda82
uzdik <uzdik.kz@gmail.com>
2024-05-20 22:19:45 +0900

    Updated app.py

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import JavascriptException, TimeoutException
-from werkzeug.utils import url_quote
-#from urllib.parse import quote as url_quote
+from urllib.parse import quote as url_quote
 from datetime import datetime
 import re
 from bs4 import BeautifulSoup
